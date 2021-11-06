0c74b6e5c96a6f88852f2c45535a6d7f8580d8fa
Елманов Юрий Алексеевич <yuelmanov@phoenixit.ru>
2021-11-07 02:12:12 +0300

    add ui components - Title.tsx, Subtitle.tsx
    Closes #43, Closes #42

---
+++ src/components/ui/Subtitle/Subtitle.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react';
+import './Subtitle.scss';
+
+interface SubtitleProps {
+  extendClass?: string,
+  text: string,
+  theme?: 'dark' | 'light',
+}
+
+export const Subtitle: FC<SubtitleProps> = ({
+                                              extendClass = '',
+                                              text,
+                                              theme = 'light' }: SubtitleProps) => {
+  return (
+    <h2
+      className={
+        'ui-subtitle text-center'
+        + `ui-subtitle--${theme}`
+        + `${extendClass ? ` ${extendClass}` : ''}`
+      }
+    >
+      { text }
+    </h2>
+  );
+};
---
+++ src/components/ui/Title/Title.tsx
@@ -0,0 +1,25 @@
+import React, { FC,  } from 'react';
+import './Title.scss';
+
+interface TitleProps {
+  extendClass?: string,
+  text: string,
+  theme?: 'dark' | 'light',
+}
+
+export const Title: FC<TitleProps> = ({
+                                              extendClass = '',
+                                              text,
+                                              theme = 'light' }: TitleProps) => {
+  return (
+    <h1
+      className={
+        'ui-title text-center'
+        + `ui-title--${theme}`
+        + `${extendClass ? ` ${extendClass}` : ''}`
+      }
+    >
+      { text }
+    </h1>
+  );
+};
