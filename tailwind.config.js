65563afd36677df7416587cc37259ab0c1b0ddd1
tenutes <leopard19670508>
2021-11-06 10:21:15 +0300

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.jsx', './src/**/*.ts', './src/**/*.tsx'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
