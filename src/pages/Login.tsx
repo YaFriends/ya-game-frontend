7198c349b7173de7d61efbf4827b4962523c0eef
Елманов Юрий Алексеевич <yuelmanov@phoenixit.ru>
2021-11-07 17:36:10 +0300

    Add Login.tsx layout

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,51 @@
-import React, { FC } from 'react';
+import React, { FC, MouseEvent } from 'react';
+import { Title } from '../components/ui/Title/Title';
+import { Input } from '../components/ui/Input/Input';
+import { Button } from '../components/ui/Button/Button';
+import { MainLink } from '../components/ui/Link/Link';
 
 export const Login: FC<{}> = () => {
+  const handleClick = (e: MouseEvent) => {
+    console.log('handleClick', e);
+  };
+
   return (
-    <section>Login</section>
+    <section className={ 'h-screen w-[354px] mx-auto flex items-center justify-center flex-col' }>
+      <div className={ 'w-full flex items-center justify-between mb-6' }>
+        <Title text="Авторизация" />
+        <MainLink text="Регистрация" href="/register"/>
+      </div>
+      <form
+        className="w-full"
+        name="authForm"
+      >
+        <div className="mb-6">
+          <Input
+            name="login"
+            label="Логин"
+            placeholder="Введите логин"
+            required
+          />
+        </div>
+        <Input
+          name="password"
+          label="Пароль"
+          placeholder="Введите пароль"
+          required
+        />
+
+        <div className="mt-14">
+          <Button
+            name="auth"
+            type="submit"
+            text="Войти"
+            click={ handleClick }
+          />
+        </div>
+      </form>
+      <div className="w-full flex items-center justify-center mt-4">
+        <MainLink text="Вернуться на главную" href="/"/>
+      </div>
+    </section>
   );
 };
