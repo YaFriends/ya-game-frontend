a46b845c61c228b327a7d4f1132caa1fd33053c2
venmovs <vigen94@icloud.com>
2021-11-08 08:59:11 +0300

    add styles

--- src/components/ui/Select/Select.tsx
+++ src/components/ui/Select/Select.tsx
@@ -32,10 +32,31 @@ const Select: FC<SelectProps> = (
         return <Option { ...props } children={ children } key={ option.value }/>;
     });
 
+    const labelClasses = 'font-bold text-lg text-black mb-1'; //TODO импортировать эти стили, так как в инпуте схожие
+    const selectClasses = [
+        'bg-black',
+        'text-white',
+        'h-[38px]',
+        'rounded-12px',
+        'border-2',
+        'border-opacity-40',
+        'border-white',
+        'text-white',
+        'placeholder-white px-3',
+        'px-2',
+        'focus:outline-none',
+        'focus:border-blue',
+        'focus:rounded-b-none',
+        'hover:cursor-pointer'
+    ].join(' ');
+
     return (
         <div className='flex flex-col'>
-            <label>{ label }</label>
+            <label className={ labelClasses } htmlFor={ id }>
+                { label }
+            </label>
             <select
+                className={ selectClasses }
                 required={ require }
                 name={ name }
                 id={ id }
