e0ec8a1a0bc11fc1c9285439f6785d62affe6810
Елманов Юрий Алексеевич <yuelmanov@phoenixit.ru>
2021-11-08 17:37:29 +0300

    back menu to dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import { Menu } from './components/Menu';
 import { Dashboard } from './pages/Dashboard';
 import { Error404 } from './pages/Error404';
 import { Forum } from './pages/Forum';
@@ -16,26 +15,23 @@ import { Register } from './pages/Register';
 
 const App: FC<Record<string, never>> = () => {
   return (
-    <div className="font-body">
-      <Router>
-        <Menu />
-        <main>
-          <Switch>
-            <Route path="/" exact component={Dashboard} />
-            <Route path="/forum" component={Forum} />
-            <Route path="/game/create" component={GameCreation} />
-            <Route path="/game/lobby" component={GameLobby} />
-            <Route path="/game/:id" component={GameSession} />
-            <Route path="/leaderboard" component={Leaderboard} />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-            <Route path="/profile/history" component={ProfileHistory} />
-            <Route path="/profile" component={Profile} />
-            <Route path="*" component={Error404} />
-          </Switch>
-        </main>
-      </Router>
-    </div>
+    <Router>
+      <main className="font-body">
+        <Switch>
+          <Route path="/" exact component={Dashboard} />
+          <Route path="/forum" component={Forum} />
+          <Route path="/game/create" component={GameCreation} />
+          <Route path="/game/lobby" component={GameLobby} />
+          <Route path="/game/:id" component={GameSession} />
+          <Route path="/leaderboard" component={Leaderboard} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/profile/history" component={ProfileHistory} />
+          <Route path="/profile" component={Profile} />
+          <Route path="*" component={Error404} />
+        </Switch>
+      </main>
+    </Router>
   );
 };
 
