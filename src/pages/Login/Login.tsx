b914925c9f2022c6d14b4dc96232e9d393342cac
Елманов Юрий Алексеевич <yuelmanov@phoenixit.ru>
2021-11-09 08:12:53 +0300

    rename class & update translation.ts

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -7,6 +7,8 @@ import { Title } from '../../components/ui/Title/Title';
 
 import './Login.scss';
 
+import { TRANSLATION } from '../../lang/ru/translation';
+
 export const Login: FC<Record<string, never>> = () => {
   const handleLogin = (e: MouseEvent) => {
     console.log('handleLogin', e);
@@ -15,21 +17,36 @@ export const Login: FC<Record<string, never>> = () => {
   return (
     <section className="login">
       <div className="login__header">
-        <Title text="Авторизация" />
-        <MainLink text="Регистрация" href="/register" />
+        <Title text={TRANSLATION.Login.title} />
+        <MainLink text={TRANSLATION.Login.linkToRegisterText} href="/register" />
       </div>
       <form className="login__form" name="loginForm">
         <div className="login__form-input-wrapper">
-          <Input name="login" label="Логин" placeholder="Введите логин" required />
+          <Input
+            name="login"
+            label={TRANSLATION.Login.inputLoginLabel}
+            placeholder={TRANSLATION.Login.inputLoginPlaceholder}
+            required
+          />
         </div>
-        <Input name="password" label="Пароль" placeholder="Введите пароль" required />
+        <Input
+          name="password"
+          label={TRANSLATION.Login.inputPasswordLabel}
+          placeholder={TRANSLATION.Login.inputPasswordPlaceholder}
+          required
+        />
 
         <div className="login__form-button-wrapper">
-          <Button name="login" type="submit" text="Войти" click={handleLogin} />
+          <Button
+            name="login"
+            type="submit"
+            text={TRANSLATION.Login.submitButtonText}
+            click={handleLogin}
+          />
         </div>
       </form>
-      <div className="login__link-wrapper">
-        <MainLink text="Вернуться на главную" href="/" />
+      <div className="login__link">
+        <MainLink text={TRANSLATION.Login.linkToDashboardText} href="/" />
       </div>
     </section>
   );
