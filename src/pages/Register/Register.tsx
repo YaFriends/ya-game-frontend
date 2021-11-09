d06bdafe3f9de57b9c98066de54320633d288adb
Елманов Юрий Алексеевич <yuelmanov@phoenixit.ru>
2021-11-09 11:24:42 +0300

    review

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,60 +1,17 @@
-import React, { FC, MouseEvent } from 'react';
+import React, { FC, FormEvent } from 'react';
 
-import { ButtonProps } from '../../components/ui/Button/Button';
+import { Button } from '../../components/ui/Button/Button';
 import { Form } from '../../components/ui/Form/Form';
-import { InputProps } from '../../components/ui/Input/Input';
+import { Input } from '../../components/ui/Input/Input';
 import { MainLink } from '../../components/ui/Link/Link';
 import { Title } from '../../components/ui/Title/Title';
 import './Register.scss';
 import { TRANSLATION } from '../../lang/ru/translation';
 
 export const Register: FC<Record<string, never>> = () => {
-  const handleRegister = (e: MouseEvent) => {
-    console.log('handleRegister', e);
-  };
-  const inputs: InputProps[] = [
-    {
-      name: 'input-first-name',
-      label: TRANSLATION.Register.inputFirstNameLabel,
-      placeholder: TRANSLATION.Register.inputFirstNamePlaceholder,
-      required: true,
-    },
-    {
-      name: 'input-second-name',
-      label: TRANSLATION.Register.inputSecondNameLabel,
-      placeholder: TRANSLATION.Register.inputSecondNamePlaceholder,
-      required: true,
-    },
-    {
-      name: 'input-login',
-      label: TRANSLATION.Register.inputLoginLabel,
-      placeholder: TRANSLATION.Register.inputLoginPlaceholder,
-      required: true,
-    },
-    {
-      name: 'input-email',
-      label: TRANSLATION.Register.inputEmailLabel,
-      placeholder: TRANSLATION.Register.inputEmailPlaceholder,
-      required: true,
-    },
-    {
-      name: 'input-phone',
-      label: TRANSLATION.Register.inputPhoneLabel,
-      placeholder: TRANSLATION.Register.inputPhonePlaceholder,
-      required: true,
-    },
-    {
-      name: 'input-password',
-      label: TRANSLATION.Register.inputPasswordLabel,
-      placeholder: TRANSLATION.Register.inputPasswordPlaceholder,
-      required: true,
-    },
-  ];
-  const button: ButtonProps = {
-    type: 'submit',
-    name: 'button-register',
-    text: TRANSLATION.Register.submitButtonText,
-    click: handleRegister,
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log('handleSubmit', e);
   };
 
   return (
@@ -63,7 +20,53 @@ export const Register: FC<Record<string, never>> = () => {
         <MainLink text="Назад" href="/login" />
         <Title text="Регистрация" />
       </div>
-      <Form name="registerForm" inputs={inputs} button={button} />
+      <Form name="registerForm" submit={handleSubmit}>
+        <div>
+          <Input
+            name="input-first-name"
+            label={TRANSLATION.Register.inputFirstNameLabel}
+            placeholder={TRANSLATION.Register.inputFirstNamePlaceholder}
+            required
+          />
+          <Input
+            name="input-second-name"
+            label={TRANSLATION.Register.inputSecondNameLabel}
+            placeholder={TRANSLATION.Register.inputSecondNamePlaceholder}
+            required
+          />
+          <Input
+            name="input-login"
+            label={TRANSLATION.Register.inputLoginLabel}
+            placeholder={TRANSLATION.Register.inputLoginPlaceholder}
+            required
+          />
+          <Input
+            name="input-email"
+            label={TRANSLATION.Register.inputEmailLabel}
+            placeholder={TRANSLATION.Register.inputEmailPlaceholder}
+            required
+          />
+          <Input
+            name="input-phone"
+            label={TRANSLATION.Register.inputPhoneLabel}
+            placeholder={TRANSLATION.Register.inputPhonePlaceholder}
+            required
+          />
+          <Input
+            name="input-password"
+            label={TRANSLATION.Register.inputPasswordLabel}
+            placeholder={TRANSLATION.Register.inputPasswordPlaceholder}
+            required
+          />
+        </div>
+        <div className="register__button">
+          <Button
+            name="button-register"
+            type="submit"
+            text={TRANSLATION.Register.submitButtonText}
+          />
+        </div>
+      </Form>
     </section>
   );
 };
