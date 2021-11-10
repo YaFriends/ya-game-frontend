35ba7e3f1444c5dd7ede26265df27c3002bd27c4
Yurik "YurikEz" Elmanov <elmanovyurik@gmail.com>
2021-11-10 19:55:26 +0300

    add Register.tsx layout (#53)
    * review
    
    * review
    
    * review
    
    * add ui component Button
    
    * review
    
    * add ui component - Button.tsx
    
    Closes #45
    
    * updated ui component - Button.tsx
    
    * add ui components - Title.tsx, Subtitle.tsx
    
    Closes #43, Closes #42
    
    * updated ui components - Title.tsx, Subtitle.tsx
    
    Closes #43, Closes #42
    
    * add empty line in end
    
    * review ui component Title.tsx
    
    * add ui components Text.tsx, Description.tsx, Link.tsx
    
    Closes #49, Closes #50, Closes #51
    
    * updated ui component Link.scss
    
    * Up classes, tailwind
    
    * Up input styles
    
    * review
    
    * review
    
    * add 100% width for Input.tsx
    
    * review link
    
    * add historyApiFallback
    
    * updated Button.tsx
    
    * Add Login.tsx layout
    
    * review className
    
    * lint
    
    * fix review
    
    * Ревью по комментам от ментора
    
    - Вынес дефолтные классы вне функций компонентов в константы
    - Заюзал useMemo
    - Создал мелкие Element'ы для некоторых компонентов
    - Ревью
    - Переписал Link
    - Вынес меню только на главную страницу
    
    * lint
    
    * lint
    
    * Up undefined
    
    * Up undefined
    
    * back menu to dashboard
    
    * review style
    
    * add Register.tsx page layout
    
    * fix names
    
    * fix
    
    * rename handle click
    
    * rename class & update translation.ts
    
    * add ui-component Form.tsx
    
    * fix review
    
    * review code
    
    * review
    
    Co-authored-by: Елманов Юрий Алексеевич <yuelmanov@phoenixit.ru>
    Co-authored-by: tenutes <leopard19670508>

--- src/lang/ru/translation.ts
+++ src/lang/ru/translation.ts
@@ -1,6 +1,21 @@
 export const TRANSLATION: Record<string, Record<string, string>> = {
   Register: {
     menuLabel: 'Регистрация',
+    title: 'Регистрация',
+    linkToLogin: 'Назад',
+    inputFirstNameLabel: 'Имя',
+    inputFirstNamePlaceholder: 'Введите имя',
+    inputSecondNameLabel: 'Фамилия',
+    inputSecondNamePlaceholder: 'Введите фамилию',
+    inputLoginLabel: 'Логин',
+    inputLoginPlaceholder: 'Введите логин',
+    inputEmailLabel: 'Email',
+    inputEmailPlaceholder: 'Введите email',
+    inputPhoneLabel: 'Телефон',
+    inputPhonePlaceholder: 'Введите телефон',
+    inputPasswordLabel: 'Пароль',
+    inputPasswordPlaceholder: 'Введите пароль',
+    submitButtonText: 'Зарегистрироваться',
   },
   Login: {
     menuLabel: 'Логин',
--- src/pages/Register.tsx
+++
@@ -1,5 +0,0 @@
-import React, { FC } from 'react';
-
-export const Register: FC<Record<string, never>> = () => {
-  return <section>Register</section>;
-};
