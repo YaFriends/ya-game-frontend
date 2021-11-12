f962fb82ad9c1db1f46a662a56631faf85cfc730
venmovs <vigen94@icloud.com>
2021-11-12 08:57:34 +0300

    remove state in Profile

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,40 +1,39 @@
-import React, { FC, useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link, useRouteMatch } from 'react-router-dom';
+import React, { FC } from 'react';
+import { BrowserRouter as Router, Route, Switch, useRouteMatch } from 'react-router-dom';
+
+import { MainLink } from '../../components/ui/Link/Link';
 
 import { Info } from './Info';
 import { Password } from './Password';
 
-type stepTypes = 'profile' | 'edit-profile' | 'password';
-
 export const Profile: FC<Record<string, never>> = () => {
-  const [step, setStep] = useState<stepTypes>('profile');
-
   const { path, url } = useRouteMatch();
 
-  useEffect(() => {
-    console.log(step);
-  }, [step]);
-
   return (
-    <section>
-      <Router>
+    <Router>
+      <section className="flex">
         <div>
           тут аватарка и тд
-          <button onClick={() => setStep('edit-profile')}>поменять на изменяемую область</button>
-          <Link to={`${url}/password`}>Изменить пароль</Link>
+          <MainLink
+            text="Изменить данные"
+            href={`${url}/info`}
+            extendClass="ui-link--button mb-6"
+          />
+          <MainLink text="Изменить пароль" href={`${url}/password`} extendClass="ui-link--button" />
+          <MainLink text="Назад" href="/" />
         </div>
         <Switch>
-          <Route path={`${path}`} exact>
+          <Route path={path} exact>
             <Info disabled={true} />
           </Route>
-          <Route path={`${path}/edit-profile`}>
-            <Info disabled={true} />
+          <Route path={`${path}/info`}>
+            <Info disabled={false} />
           </Route>
-          <Route path={`${path}/password`} exact>
+          <Route path={`${path}/password`}>
             <Password />
           </Route>
         </Switch>
-      </Router>
-    </section>
+      </section>
+    </Router>
   );
 };
