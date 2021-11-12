65d6bea4b9f8be9ab9398edb6f7b1ea70c53f52d
tenutes <leopard19670508>
2021-11-12 10:18:05 +0300

    Fix rounting

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import { BrowserRouter as Router, Route, Switch, useRouteMatch } from 'react-router-dom';
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
 
 import { MainLink } from '../../components/ui/Link/Link';
 
@@ -8,32 +8,25 @@ import { Password } from './Password';
 
 export const Profile: FC<Record<string, never>> = () => {
   const { path, url } = useRouteMatch();
-
   return (
-    <Router>
-      <section className="flex">
-        <div>
-          тут аватарка и тд
-          <MainLink
-            text="Изменить данные"
-            href={`${url}/info`}
-            extendClass="ui-link--button mb-6"
-          />
-          <MainLink text="Изменить пароль" href={`${url}/password`} extendClass="ui-link--button" />
-          <MainLink text="Назад" href="/" />
-        </div>
-        <Switch>
-          <Route path={path} exact>
-            <Info disabled={true} />
-          </Route>
-          <Route path={`${path}/info`}>
-            <Info disabled={false} />
-          </Route>
-          <Route path={`${path}/password`}>
-            <Password />
-          </Route>
-        </Switch>
-      </section>
-    </Router>
+    <section className="flex">
+      <div>
+        тут аватарка и тд
+        <MainLink text="Изменить данные" href={`${url}/info`} extendClass="ui-link--button mb-6" />
+        <MainLink text="Изменить пароль" href={`${url}/password`} extendClass="ui-link--button" />
+        <MainLink text="Назад" href="/" />
+      </div>
+      <Switch>
+        <Route path={path} exact>
+          <Info disabled={true} />
+        </Route>
+        <Route path={`${path}/info`}>
+          <Info disabled={false} />
+        </Route>
+        <Route path={`${path}/password`}>
+          <Password />
+        </Route>
+      </Switch>
+    </section>
   );
 };
