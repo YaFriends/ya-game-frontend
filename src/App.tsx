7a96558b71c2effb42a4b5b0ff5317a5d4c313c4
Елманов Юрий Алексеевич <yuelmanov@phoenixit.ru>
2021-11-15 14:17:34 +0300

    Переработал точку входа
    - убрал стор и провайдер выше
    - настроил редиректы
    - проверяю авторизован ли пользователь или нет
    
    Closes #56

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React, { FC } from 'react';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React, { FC, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
+import { AuthController } from './controllers/AuthController';
 import { Dashboard } from './pages/Dashboard/Dashboard';
 import { Error404 } from './pages/Error404';
 import { Forum } from './pages/Forum';
@@ -10,32 +10,51 @@ import { GameLobby } from './pages/GameLobby';
 import { GameSession } from './pages/GameSession';
 import { Leaderboard } from './pages/Leaderboard/Leaderboard';
 import { Login } from './pages/Login/Login';
+import { Main } from './pages/Main/Main';
 import { Profile } from './pages/Profile';
 import { ProfileHistory } from './pages/ProfileHistory/ProfileHistory';
 import { Register } from './pages/Register/Register';
-import { store } from './store';
+import { useAppSelector, useAppDispatch } from './store/hooks';
+import { authActions } from './store/slices/authSlice';
 
 const App: FC<Record<string, never>> = () => {
+  const dispatch = useAppDispatch();
+  const currentUser = useAppSelector(state => state.auth.currentUser);
+
+  // TODO: Написать свой хук для проверки авторизации
+  useEffect(() => {
+    if (!currentUser) {
+      AuthController.fetchUser().then(response => dispatch(authActions.setCurrentUser(response)));
+    }
+  }, [currentUser]);
+
   return (
-    <Provider store={store}>
-      <Router>
-        <main className="font-body text-black container game-container">
-          <Switch>
-            <Route path="/" exact component={Dashboard} />
-            <Route path="/forum" component={Forum} />
-            <Route path="/game/create" component={GameCreation} />
-            <Route path="/game/lobby" component={GameLobby} />
-            <Route path="/game/:id" component={GameSession} />
-            <Route path="/leaderboard" component={Leaderboard} />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-            <Route path="/profile/history" component={ProfileHistory} />
-            <Route path="/profile" component={Profile} />
-            <Route path="*" component={Error404} />
-          </Switch>
-        </main>
-      </Router>
-    </Provider>
+    <Router>
+      <main className="font-body text-black container game-container">
+        <Switch>
+          <Route path="/login">{currentUser ? <Redirect to="/" /> : <Login />}</Route>
+          <Route path="/register">{currentUser ? <Redirect to="/" /> : <Register />}</Route>
+          <Route path="/main" component={Main} />
+          <Route path="/" exact>
+            {currentUser ? <Dashboard /> : <Redirect to="/login" />}
+          </Route>
+          <Route path="/forum">{currentUser ? <Forum /> : <Redirect to="/login" />}</Route>
+          <Route path="/game/create">
+            {currentUser ? <GameCreation /> : <Redirect to="/login" />}
+          </Route>
+          <Route path="/game/lobby">{currentUser ? <GameLobby /> : <Redirect to="/login" />}</Route>
+          <Route path="/game/:id">{currentUser ? <GameSession /> : <Redirect to="/login" />}</Route>
+          <Route path="/leaderboard">
+            {currentUser ? <Leaderboard /> : <Redirect to="/login" />}
+          </Route>
+          <Route path="/profile/history">
+            {currentUser ? <ProfileHistory /> : <Redirect to="/login" />}
+          </Route>
+          <Route path="/profile">{currentUser ? <Profile /> : <Redirect to="/login" />}</Route>
+          <Route path="*" component={Error404} />
+        </Switch>
+      </main>
+    </Router>
   );
 };
 
