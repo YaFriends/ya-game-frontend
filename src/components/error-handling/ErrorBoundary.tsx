14db40c81c5fb84e98573927b75a7d163617210a
venmovs <vigen94@icloud.com>
2021-11-16 07:40:16 +0300

    changed exports

--- src/components/error-handling/ErrorBoundary.tsx
+++ src/components/error-handling/ErrorBoundary.tsx
@@ -1,99 +1,85 @@
-import React, {
-    Component,
-    ComponentType,
-    ErrorInfo,
-    PropsWithChildren
-} from 'react';
+import React, { Component, ComponentType, ErrorInfo, PropsWithChildren } from 'react';
 
 const isArrayChange = (a: unknown[] = [], b: unknown[] = []) =>
-    a.length !== b.length || a.some((item, index) => !Object.is(item, b[index]));
+  a.length !== b.length || a.some((item, index) => !Object.is(item, b[index]));
 
-interface FallbackProps {
-    error: Error;
-    resetErrorBoundary: (...args: unknown[]) => void;
+export interface FallbackProps {
+  error: Error;
+  resetErrorBoundary: (...args: unknown[]) => void;
 }
 
 interface ErrorBoundaryPropsWithComponent {
-    onResetKeysChange?: (
-        prevResetKeys: unknown[] | undefined,
-        resetKeys: unknown[] | undefined,
-    ) => void;
-    onReset?: (...args: unknown[]) => void;
-    onError?: (error: Error, info: { componentStack: string }) => void;
-    resetKeys?: unknown[];
-    FallbackComponent: ComponentType<FallbackProps>;
+  onResetKeysChange?: (
+    prevResetKeys: unknown[] | undefined,
+    resetKeys: unknown[] | undefined
+  ) => void;
+  onReset?: (...args: unknown[]) => void;
+  onError?: (error: Error, info: { componentStack: string }) => void;
+  resetKeys?: unknown[];
+  FallbackComponent: ComponentType<FallbackProps>;
 }
 
 type ErrorBoundaryState = { error: Error | null };
 
-class ErrorBoundary extends Component<PropsWithChildren<ErrorBoundaryPropsWithComponent>,
-    ErrorBoundaryState>
-{
-    static getDerivedStateFromError(error: Error) {
-        return { error }
-    }
+export class ErrorBoundary extends Component<
+  PropsWithChildren<ErrorBoundaryPropsWithComponent>,
+  ErrorBoundaryState
+> {
+  static getDerivedStateFromError(error: Error) {
+    return { error };
+  }
 
-    state: ErrorBoundaryState = {
-        error: null
-    };
+  state: ErrorBoundaryState = {
+    error: null,
+  };
 
-    resetErrorBoundary = (...args: unknown[]) => {
-        this.props.onReset?.(...args);
-        this.reset();
-    };
+  resetErrorBoundary = (...args: unknown[]) => {
+    this.props.onReset?.(...args);
+    this.reset();
+  };
 
-    reset() {
-        this.setState(() => {
-            return {
-                error: null
-            }
-        });
-    }
+  reset() {
+    this.setState(() => {
+      return {
+        error: null,
+      };
+    });
+  }
 
-    componentDidCatch(error: Error, info: ErrorInfo) {
-        this.props.onError?.(error, info);
-    }
+  componentDidCatch(error: Error, info: ErrorInfo) {
+    this.props.onError?.(error, info);
+  }
 
-    componentDidUpdate(
-        prevProps: ErrorBoundaryPropsWithComponent,
-        prevState: ErrorBoundaryState,
+  componentDidUpdate(prevProps: ErrorBoundaryPropsWithComponent, prevState: ErrorBoundaryState) {
+    const { error } = this.state;
+    const { resetKeys } = this.props;
+
+    if (
+      error !== null &&
+      prevState.error !== null &&
+      isArrayChange(prevProps.resetKeys, resetKeys)
     ) {
-        const { error } = this.state;
-        const { resetKeys } = this.props;
+      this.props.onResetKeysChange?.(prevProps.resetKeys, resetKeys);
+      this.reset();
+    }
+  }
 
-        if (
-            error !== null &&
-            prevState.error !== null &&
-            isArrayChange(prevProps.resetKeys, resetKeys)
-        ) {
-            this.props.onResetKeysChange?.(prevProps.resetKeys, resetKeys);
-            this.reset();
-        }
+  render() {
+    const { error } = this.state;
+
+    const { FallbackComponent } = this.props;
+
+    if (error !== null) {
+      const props = {
+        error,
+        resetErrorBoundary: this.resetErrorBoundary,
+      };
+      if (FallbackComponent) {
+        return <FallbackComponent {...props} />;
+      }
+      throw new Error('Requires a FallbackComponent prop');
     }
 
-    render() {
-        const { error } = this.state;
-
-        const { FallbackComponent } = this.props;
-
-        if (error !== null) {
-            const props = {
-                error,
-                resetErrorBoundary: this.resetErrorBoundary,
-            };
-            if (FallbackComponent) {
-                return <FallbackComponent {...props} />
-            } else {
-                throw new Error('Requires a FallbackComponent prop');
-            }
-        }
-
-        return this.props.children;
-    }
+    return this.props.children;
+  }
 }
-
-export type { FallbackProps };
-
-export { ErrorBoundary };
-
-
--- src/components/error-handling/ErrorFallback.tsx
+++ src/components/error-handling/ErrorFallback.tsx
@@ -1,17 +1,19 @@
 import React, { FC } from 'react';
-import { FallbackProps } from './ErrorBoundary';
+
 import { TRANSLATION } from '../../lang/ru/translation';
 
-const ErrorFallback: FC<FallbackProps> = ({error, resetErrorBoundary}) => {
-    return (
-        <div role="alert">
-            <p>{ TRANSLATION.SomethingWentWrong }</p>
-            <pre>{error.message || error}</pre>
-            <button type='button' onClick={resetErrorBoundary}>
-                { TRANSLATION.TryAgain }
-            </button>
-        </div>
-    )
+import { FallbackProps } from './ErrorBoundary';
+
+const ErrorFallback: FC<FallbackProps> = ({ error, resetErrorBoundary }) => {
+  return (
+    <div role="alert">
+      <p>{TRANSLATION.SomethingWentWrong}</p>
+      <pre>{error.message || error}</pre>
+      <button type="button" onClick={resetErrorBoundary}>
+        {TRANSLATION.TryAgain}
+      </button>
+    </div>
+  );
 };
 
 export { ErrorFallback };
