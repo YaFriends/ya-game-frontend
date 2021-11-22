e36990b1b0e627512953d832f518ae88efdb48c0
Елманов Юрий Алексеевич <yuelmanov@phoenixit.ru>
2021-11-22 21:33:20 +0300

    add Spinner.tsx

---
+++ src/components/ui/Spinner/Spinner.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from 'react';
+
+import { Subtitle } from '../Subtitle/Subtitle';
+
+import './Spinner.scss';
+
+export const Spinner: FC<Record<string, never>> = () => {
+  return (
+    <section className="ui-spinner">
+      <div className="loading">
+        <div className="arc" />
+        <div className="arc" />
+        <div className="arc" />
+      </div>
+      <Subtitle text="Загружаем..." theme="dark" />
+    </section>
+  );
+};
