8906f1a0f529db75fa82554f6aad2220347ef01a
Елманов Юрий Алексеевич <yuelmanov@phoenixit.ru>
2021-11-23 11:07:24 +0300

    review

--- src/hooks/use-auth.ts
+++ src/hooks/use-auth.ts
@@ -1,18 +1,12 @@
-import { UserData } from '../api/UserAPI';
+import { UseAuth, CurrentUser } from '../@types/AuthTypes';
 
-import { useAppSelector } from './redux-hooks';
-
-interface UseAuth {
-  isAuth: boolean;
-  currentUser: UserData | null;
-}
+import { useAppSelector } from './redux';
 
 export const useAuth = (): UseAuth => {
-  const currentUser: UserData | null = useAppSelector(state => state.auth.currentUser);
+  const currentUser: CurrentUser = useAppSelector(state => state.auth.currentUser);
   const isAuth =
-    Boolean(currentUser) || typeof window !== 'undefined'
-      ? localStorage.getItem('isAuth') === 'true'
-      : false;
+    Boolean(currentUser) ||
+    (typeof window !== 'undefined' && localStorage.getItem('isAuth') === 'true');
 
   if (typeof window !== 'undefined') {
     localStorage.setItem('isAuth', String(isAuth));
