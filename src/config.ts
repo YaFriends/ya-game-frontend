3a67cca1106b79f513a9ea5b730f9a30e7e823bb
Yurik Elmanov <elmanovyurik@gmail.com>
2021-12-07 12:09:07 +0300

    upd

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 console.log('process', process);
-console.log('process-EXTERNAL_API_URL', process.env.API_EXTERNAL);
-console.log('process-INTERNAL_API_URL', process.env.API_INTERNAL);
+console.log('process-EXTERNAL_API_URL', process.env?.API_EXTERNAL);
+console.log('process-INTERNAL_API_URL', process.env?.API_INTERNAL);
 
-export const EXTERNAL_API_URL = process.env.API_EXTERNAL;
-export const INTERNAL_API_URL = process.env.API_INTERNAL || 'http://localhost';
+export const EXTERNAL_API_URL = process.env?.API_EXTERNAL;
+export const INTERNAL_API_URL = process.env?.API_INTERNAL || 'http://localhost';
 
 console.log('EXTERNAL_API_URL', EXTERNAL_API_URL);
 console.log('INTERNAL_API_URL', INTERNAL_API_URL);
