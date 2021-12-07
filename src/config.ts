24dac966372285303c92ef098d2fe12648e635bb
Yurik Elmanov <elmanovyurik@gmail.com>
2021-12-07 17:24:18 +0300

    upd

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,6 @@ export const INTERNAL_API_URL = process.env.API_INTERNAL || 'http://localhost';
 console.log('EXTERNAL_API_URL', EXTERNAL_API_URL);
 console.log('NODE_ENV', process.env.NODE_ENV);
 console.log('TEST_KEK', process.env.TEST_KEK);
+console.log('TEST_PISJA', process.env.TEST_PISJA);
+console.log('TEST_PISJA3', process.env.TEST_PISJA3);
 console.log('INTERNAL_API_URL', INTERNAL_API_URL);
---
+++ webpack.prod.js
@@ -0,0 +1,78 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+require('dotenv-webpack');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  devServer: {
+    hot: true,
+    open: true,
+    historyApiFallback: true,
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    fallback: {
+      "fs": false,
+      "path": false,
+      "http": false,
+      "https": false,
+      "zlib": false,
+      "os": false,
+      "tty": require.resolve('tty-browserify'),
+      "timers": false,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            },
+          },
+          'postcss-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new ESLintPlugin({
+      extensions: ['ts', 'tsx'],
+      files: ['src'],
+      fix: true,
+    }),
+    new CopyPlugin({
+      patterns: [{ from: 'static', to: 'static' }],
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV !== 'production' ? 'development' : 'production'),
+        'API_EXTERNAL': JSON.stringify(process.env.API_EXTERNAL),
+        'TEST_KEK': JSON.stringify(process.env.TEST_KEK),
+        'TEST_PISJA': JSON.stringify('TEST_PISJA2'),
+        'TEST_PISJA3': JSON.stringify('TEST_PISJA23'),
+        'API_INTERNAL': JSON.stringify(process.env.API_INTERNAL),
+      },
+    }),
+  ],
+};
