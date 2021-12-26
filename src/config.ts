a3f872e9cba87526d8e00fb956756e7243606754
Yurik Elmanov <elmanovyurik@gmail.com>
2021-12-26 17:28:15 +0300

    review

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,8 @@
+const isDev = process.env.NODE_ENV !== 'production';
 export const EXTERNAL_API_URL = 'https://ya-praktikum.tech/api/v2';
-export const INTERNAL_API_URL =
-  process.env.NODE_ENV !== 'production'
-    ? 'http://localhost:8000'
-    : 'https://ya-friends-demo-1.herokuapp.com';
-export const REDIRECT_URI_FOR_OAUTH =
-  process.env.NODE_ENV !== 'production'
-    ? 'http://localhost:8000'
-    : 'https://ya-friends-demo-1.herokuapp.com';
+export const INTERNAL_API_URL = isDev
+  ? 'http://localhost:8000'
+  : 'https://ya-friends-demo-1.herokuapp.com';
+export const REDIRECT_URI_FOR_OAUTH = isDev
+  ? 'http://localhost:8000'
+  : 'https://ya-friends-demo-1.herokuapp.com';
