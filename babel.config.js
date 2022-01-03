297292cee79f21d332980fc4c180b02ca612fb28
Виген Мовсисян <venmovs@MacBook-Pro-Vigen.local>
2022-01-03 13:46:59 +0300

    fixed indentation

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,12 @@
 module.exports = {
     presets: [
-        ['@babel/preset-env', {targets: {node: 'current'}}],
+        ['@babel/preset-env', { targets: { node: 'current' } }],
         '@babel/preset-typescript',
-        '@babel/preset-react'
+        '@babel/preset-react',
     ],
-    plugins: [
+        plugins: [
         'react-hot-loader/babel',
         '@babel/plugin-proposal-class-properties',
         '@babel/plugin-syntax-dynamic-import',
-    ]
+    ],
 };
