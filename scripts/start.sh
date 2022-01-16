b6faa348e96b9a2232fb691d247a330cbbfa69b3
tenutes <leopard19670508>
2022-01-16 20:13:23 +0300

    Final hotfixes

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 if [ "$NODE_ENV" = "production" ] ; then
-  yarn install --frozen-lockfile && yarn build && npx msw init dist/ --save
-  node server.js
+  yarn build && npx msw init dist/ && node server.js
 else
-  yarn install
   yarn start
 fi
