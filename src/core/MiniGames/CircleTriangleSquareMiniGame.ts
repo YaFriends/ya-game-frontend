2bd0efde614a87815c2b2411762733a1089118fd
Yurik "YurikEz" Elmanov <elmanovyurik@gmail.com>
2022-01-23 00:52:36 +0300

    add new game (Круг Треугольник Квадрат) (#130)
    * stash
    
    * stash
    
    * add game
    
    #closes 106
    
    * fix port frontend
    
    * review

--- src/core/MiniGames/CircleTriangleSquareMiniGame.ts
+++ src/core/MiniGames/CircleTriangleSquareMiniGame.ts
@@ -1,16 +1,31 @@
+import { FinishFn, MiniGameFinishResponse, Rivals } from '../../@types/MiniGame';
+import { UserData } from '../../@types/UserTypes';
 import { TRANSLATION } from '../../lang/ru/translation';
 import { GameLoop } from '../GameLoop';
 import MiniGame from '../MiniGame';
-import { FinishFn, MiniGameFinishResponse, Rivals } from '../../@types/MiniGame';
+
+type itemProps = {
+  id: number;
+  name: string;
+  label: string;
+  src: string;
+};
 
 type CircleTriangleSquareProps = {
   players: Rivals;
   canvasId: string;
 };
 
+const FONT_FOR_TEXT = 'bold 24px sans-serif';
+
+// Игрок выбирает одну из 3 корточек на след. экране показывается выбор игроков и далее на след экране, кто победил и снова по кругу до 3 побед
 export class CircleTriangleSquareMiniGame extends MiniGame {
   finishCb: FinishFn | null;
   loop: GameLoop;
+  items: itemProps[];
+  roundsWinForWin: number;
+  currentPlayerScore: number;
+  enemyPlayerScore: number;
 
   constructor(props: CircleTriangleSquareProps) {
     super({
@@ -19,6 +34,30 @@ export class CircleTriangleSquareMiniGame extends MiniGame {
       ...props,
     });
 
+    this.items = [
+      {
+        id: 1,
+        name: 'circle',
+        label: TRANSLATION.CircleTriangleSquare.circle,
+        src: '/static/img/games/circle_triangle_square/circle.jpeg',
+      },
+      {
+        id: 2,
+        name: 'triangle',
+        label: TRANSLATION.CircleTriangleSquare.triangle,
+        src: '/static/img/games/circle_triangle_square/triangle.jpeg',
+      },
+      {
+        id: 3,
+        name: 'square',
+        label: TRANSLATION.CircleTriangleSquare.square,
+        src: '/static/img/games/circle_triangle_square/square.jpeg',
+      },
+    ];
+    this.roundsWinForWin = 3;
+    this.currentPlayerScore = 0;
+    this.enemyPlayerScore = 0;
+
     this.loop = new GameLoop('canvas');
     this.finishCb = () => null;
   }
@@ -26,31 +65,184 @@ export class CircleTriangleSquareMiniGame extends MiniGame {
   run() {
     return new Promise<MiniGameFinishResponse>(res => {
       this.finishCb = res;
+      this.draw();
     });
   }
 
   draw() {
-    console.log('draw');
+    this._initStageStartGame();
   }
 
-  makeTurn() {
-    console.log('makeTurn');
-  }
-
-  finish() {
+  finish(player: UserData) {
     if (typeof this.finishCb === 'function') {
       this.finishCb({
-        winner: {
-          login: 'TeViYu',
-          id: 1,
-          first_name: 'Test',
-          second_name: 'test 1',
-          display_name: 'Testovich',
-          email: 'string',
-          phone: 'string',
-          avatar: '',
-        },
+        winner: player,
       });
+    }
+  }
+
+  _initStageStartGame() {
+    this._addListener();
+    this._clearGameField();
+    const step = 150;
+
+    this.GameLoop.context.font = FONT_FOR_TEXT;
+    this.GameLoop.context.fillText(TRANSLATION.CircleTriangleSquare.chooseCardWithFigure, 60, 75);
+
+    this.items.forEach(({ src }, index) => {
+      const image = new Image();
+      const { height } = this.GameLoop.canvas.getBoundingClientRect();
+      image.src = src;
+
+      const drawItem = () => {
+        const x = 50 + step * index;
+        const y = height / 2 - image.height / 2;
+        this.GameLoop.context.drawImage(image, x, y);
+      };
+
+      image.addEventListener('load', drawItem);
+    });
+  }
+
+  _initStageMiddleGame(currentPlayer: number, enemyPlayer: number, isNobody = false) {
+    this._clearGameField();
+    const step = 150;
+
+    this.GameLoop.context.font = FONT_FOR_TEXT;
+
+    if (isNobody) {
+      this.GameLoop.context.fillText(TRANSLATION.CircleTriangleSquare.nobody, 210, 75);
+    } else {
+      this.GameLoop.context.fillText(TRANSLATION.CircleTriangleSquare.selectedShapes, 130, 75);
+    }
+
+    [currentPlayer, enemyPlayer].forEach((i, index) => {
+      const item = this.items.find(({ id }) => id === i);
+
+      const image = new Image();
+      const { height } = this.GameLoop.canvas.getBoundingClientRect();
+      image.src = item!.src;
+
+      const drawItem = () => {
+        const x = 125 + step * index;
+        const y = height / 2 - image.height / 2;
+        this.GameLoop.context.drawImage(image, x, y);
+      };
+
+      image.addEventListener('load', drawItem);
+    });
+  }
+  _initStageEndGame(winnerOnRound: number) {
+    this._clearGameField();
+
+    this.GameLoop.context.font = FONT_FOR_TEXT;
+    this.GameLoop.context.fillText(TRANSLATION.CircleTriangleSquare.win, 200, 75);
+
+    const item = this.items.find(({ id }) => id === winnerOnRound);
+
+    const image = new Image();
+    const { height } = this.GameLoop.canvas.getBoundingClientRect();
+    image.src = item!.src;
+
+    const drawItem = () => {
+      const x = 200;
+      const y = height / 2 - image.height / 2;
+      this.GameLoop.context.drawImage(image, x, y);
+    };
+
+    image.addEventListener('load', drawItem);
+  }
+
+  _clearGameField() {
+    this.GameLoop.context.clearRect(0, 0, this.GameLoop.canvasSize, this.GameLoop.canvasSize);
+  }
+
+  _addListener() {
+    this.GameLoop.canvas.addEventListener('mouseup', this._onMouseUp);
+  }
+
+  _removeListener() {
+    this.GameLoop.canvas.removeEventListener('mouseup', this._onMouseUp);
+  }
+
+  _onMouseUp = (event: MouseEvent) => {
+    const canvasMousePosition = this._getCanvasMousePosition(event);
+    this._chooseItem(canvasMousePosition);
+  };
+
+  _getCanvasMousePosition({ clientX, clientY }: MouseEvent) {
+    const { left, top } = this.GameLoop.canvas.getBoundingClientRect();
+
+    return {
+      x: clientX - left,
+      y: clientY - top,
+    };
+  }
+
+  _chooseItem(mouse: { x: number; y: number }) {
+    const { x, y } = mouse;
+    let choosePlayer = null;
+
+    if (y >= 200 && y <= 300) {
+      if (x >= 50 && x <= 150) {
+        choosePlayer = this.items[0].id;
+      } else if (x >= 200 && x <= 300) {
+        choosePlayer = this.items[1].id;
+      } else if (x >= 350 && x <= 450) {
+        choosePlayer = this.items[2].id;
+      }
+    }
+
+    if (choosePlayer !== null) {
+      this._removeListener();
+      this._comparisonOfChoosingResults(choosePlayer, this._randomResultEnemyPlayer());
+    }
+  }
+
+  // TODO: заменить _randomResultEnemyPlayer на функцию просчета игрока по сети
+  // Временная функция, пока нет сети (генерация выбора противника)
+  _randomResultEnemyPlayer() {
+    const max = 3;
+    return Math.floor(Math.random() * max) + 1; // Добавляем 1, чтобы отсчет был от 1, а не от 0
+  }
+
+  _comparisonOfChoosingResults(currentPlayer: number, enemyPlayer: number) {
+    const delayScreen = 1250;
+    this._initStageMiddleGame(currentPlayer, enemyPlayer);
+
+    setTimeout(() => {
+      if (currentPlayer === enemyPlayer) {
+        this._initStageMiddleGame(currentPlayer, enemyPlayer, currentPlayer === enemyPlayer);
+      } else if (currentPlayer === 1 && enemyPlayer === 2) {
+        this.currentPlayerScore += 1;
+        this._initStageEndGame(currentPlayer);
+      } else if (currentPlayer === 2 && enemyPlayer === 3) {
+        this.currentPlayerScore += 1;
+        this._initStageEndGame(currentPlayer);
+      } else if (currentPlayer === 3 && enemyPlayer === 1) {
+        this.currentPlayerScore += 1;
+        this._initStageEndGame(currentPlayer);
+      } else {
+        this.enemyPlayerScore += 1;
+        this._initStageEndGame(enemyPlayer);
+      }
+    }, delayScreen);
+
+    setTimeout(() => {
+      this._checkingWin();
+    }, delayScreen * 2);
+  }
+
+  _checkingWin() {
+    const winner =
+      this.currentPlayerScore > this.enemyPlayerScore ? this.players[0] : this.players[1];
+    if (
+      this.currentPlayerScore === this.roundsWinForWin ||
+      this.enemyPlayerScore === this.roundsWinForWin
+    ) {
+      this.finish(winner);
+    } else {
+      this._initStageStartGame();
     }
   }
 }
--- src/core/constants.ts
+++ src/core/constants.ts
@@ -1,11 +1,13 @@
 import { MiniGamePickInfo } from '../@types/MiniGame';
 
 import MiniGame from './MiniGame';
+import { CircleTriangleSquareMiniGame } from './MiniGames/CircleTriangleSquareMiniGame';
 import { ClickMoreMiniGame } from './MiniGames/ClickMoreMiniGame';
 import { TicTacToeMiniGame } from './MiniGames/TicTacToeMiniGame';
 
 export const TIC_TAC_TOE_GAME_ID = 1;
 export const CLICK_MORE_GAME_ID = 2;
+export const CIRCLE_TRIANGLE_SQUARE_GAME_ID = 3;
 
 export type SubclassOfMiniGame = (new (x: any) => MiniGame) & {
   [K in keyof typeof MiniGame]: typeof MiniGame[K];
@@ -14,6 +16,7 @@ export type SubclassOfMiniGame = (new (x: any) => MiniGame) & {
 export const MINI_GAME_CONTROLLER_BY_ID: { [x: number]: SubclassOfMiniGame } = {
   [TIC_TAC_TOE_GAME_ID]: TicTacToeMiniGame,
   [CLICK_MORE_GAME_ID]: ClickMoreMiniGame,
+  [CIRCLE_TRIANGLE_SQUARE_GAME_ID]: CircleTriangleSquareMiniGame,
 };
 
 export const MINI_GAMES: MiniGamePickInfo[] = [TicTacToeMiniGame.config];
