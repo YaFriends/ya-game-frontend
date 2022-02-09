4c230eed1fbf0c8db12a8118c87b4ba3d1381cad
Yurik Elmanov <elmanovyurik@gmail.com>
2022-02-09 15:20:10 +0300

    up

--- public/cacheServiceWorker.js
+++ public/cacheServiceWorker.js
@@ -3,35 +3,37 @@ import { precacheAndRoute } from 'workbox-precaching';
 
 precacheAndRoute(self.__WB_MANIFEST);
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then((res) => {
+      .then(res => {
         const fetchRequest = event.request.clone();
 
-        return new Promise((resolve) => {
+        return new Promise(resolve => {
           fetch(fetchRequest)
-            .then((response) => {
-              if (!response || response.status !== 200 || response.type !== 'basic') {
+            .then(response => {
+              if (!response || !response.status.toString().startsWith('2') || response.type !== 'basic') {
                 return resolve(response);
               }
 
               const responseToCache = response.clone();
 
               caches.open(cacheNames.precache)
-                .then((cache) => {
+                .then(cache => {
                   cache.put(event.request, responseToCache);
                 });
 
               return resolve(response);
             })
-            .catch((err) => {
+            .catch(err => {
               if (res) {
                 return resolve(res);
               }
+
               throw err;
             });
         });
-      }).catch(err=>{console.log(err)}),
+      })
+      .catch(err => { console.log(err) }),
   );
 });
